53a122a90611290a7010208dbbd84f39b6128273
wenlei <906447432@qq.com>
2020-09-08 09:52:56 +0800

    commit

--- basic_class_01/BubbleSort.py
+++ basic_class_01/BubbleSort.py
@@ -15,7 +15,54 @@ def bubbleSort(arr):
     for end in range(len(arr) - 1, 0, -1):
         for i in range(end):
             if arr[i] > arr[i + 1]:
-                swap(arr, i, i+1) #交换数组中的两个数
+                arr[i], arr[i + 1] = arr[i + 1], arr[i] #交换数组中的两个数
+
+# 优化1：对于[5, 1, 2, 3, 4]，第一次排序就可以排好：用一个标志位，如果一次遍历下来没有交换过元素，则表示已经排好序。
+def bubbleSort1(arr):
+    if not arr or len(arr) < 2:
+        return
+    for end in range(len(arr) - 1, 0, -1):
+        flag = True
+        for i in range(end):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                flag = False
+        if flag: break # 不用再进行下一轮排序了
+
+# 优化2：对于[4,2,1,3,5,6,7]，后半部分已经有序：记录下最后一次交换的位置，下一次比较到这里就可以
+def bubbleSort2(arr):
+    if not arr or len(arr) < 2:
+        return
+    k = len(arr) - 1
+    pos = 0
+    for i in range(len(arr) - 1):
+        flag = True
+        for j in range(k):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                pos = j + 1
+                flag = False
+        k = pos
+        if flag: break # 不用再进行下一轮排序了
+
+# 优化3：对于[6, 1, 2, 3, 4, 5, 0]，最大值和最小值分别在左右两边的情况：
+# 从左到右排完序以后，立即从右到左排序
+def bubbleSort3(arr):
+    if not arr or len(arr) < 2:
+        return
+    for i in range(len(arr) - 1):
+        flag = True
+        for j in range(len(arr) - 1 - i):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                flag = False
+        if not flag:
+            flag = True
+            for j in range(len(arr) - 2 - i, 0, -1):
+                if arr[j - 1] > arr[j]:
+                    arr[j], arr[j - 1] = arr[j - 1], arr[j]
+                    flag = False
+        if flag: break
 
 def swap(arr,a,b):
     tmp = arr[a]
@@ -65,7 +112,7 @@ if __name__ == '__main__':
     for i in range(testTime):
         arr1 = generateRandomArray(maxSize, maxValue)
         arr2 = copyArray(arr1)
-        bubbleSort(arr1)
+        bubbleSort3(arr1)
         comparator(arr2)
         if not isEqual(arr1,arr2):
             success = False
