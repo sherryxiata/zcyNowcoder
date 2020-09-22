d87e8644334cae3be02952f8788b858aefbe5805
wenlei <906447432@qq.com>
2020-09-22 13:28:28 +0800

    Merge branch 'master' of github.com:sherryxiata/zcyNowcoder
     Conflicts:
    	.idea/.gitignore
    	.idea/misc.xml
    	.idea/modules.xml
    	practice/BinarySearch.py
    	test.py

---
+++ practice/FindSecondMax.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/9/20 9:53
+# @Author  : wenlei
+
+'''
+快速找出数组中最大和次大的数
+'''
+
+def findSecondMax(nums):
+    if len(nums) < 3:return nums
+    ma, ma2 = nums[0], nums[0]
+    for num in nums:
+        if num > ma:
+            ma2 = ma
+            ma = num
+        elif ma2 < num < ma:
+            ma2 = num
+    return [ma2, ma]
+
+if __name__ == '__main__':
+    arr = [1,8,8,8,6]
+    print(findSecondMax(arr))
--- test.py
+++ test.py
@@ -1,12 +1,5 @@
-s = input()
-dict = {}
-for c in s:
-    if c in dict:
-        dict[c] += 1
-    else:
-        dict[c] = 1
-dict = sorted(dict.items(), key = lambda x: x[0])
-# print(dict)
-for num in dict:
-    print(num[0] + '=' + str(num[1]), end = ' ')
-print()
+def test(**kwargs):
+    for k, v in kwargs.items():
+        print(v)
+
+test(x = 1, y = 2)
